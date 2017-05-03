6423a980b3d9dd8c22c9d4df69bfe1471b450b49
Scott Simpson <sasimpson@gmail.com>
2017-05-02 23:48:30 -0700

    update sleep api and model

--- api/sleep.go
+++ api/sleep.go
@@ -15,17 +15,33 @@ type SleepRequest struct {
 	SleepData models.Sleep `json:"sleepData"`
 }
 
+type SleepResponse struct {
+	SleepData []models.Sleep `json:"sleepData"`
+}
+
 func initSleepHandlers(r *mux.Router) {
 	s := r.PathPrefix("/sleep").Subrouter()
-	s.HandleFunc("/", sleepGetHandler).Methods("GET")
-	s.HandleFunc("/", sleepNewHandler).Methods("POST")
+	s.HandleFunc("", sleepGetHandler).Methods("GET")
+	s.HandleFunc("", sleepNewHandler).Methods("POST")
+	s.HandleFunc("/status", sleepToggleStatus).Methods("GET")
+	s.HandleFunc("/start", sleepStartHandler).Methods("GET")
+	s.HandleFunc("/end", sleepEndHandler).Methods("GET")
 	s.HandleFunc("/{id}", sleepViewHandler).Methods("GET")
 	s.HandleFunc("/{id}", sleepEditHandler).Methods("PUT")
 	s.HandleFunc("/{id}", sleepDeleteHandler).Methods("DELETE")
 }
 
 func sleepGetHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GET no id")
+	log.Println("GET sleep")
+	var sleep models.Sleep
+	sleepData, err := sleep.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	sleepResponse := SleepResponse{SleepData: sleepData}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(sleepResponse.SleepData)
 }
 
 func sleepViewHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,6 +68,7 @@ func sleepNewHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusConflict)
+		return
 	}
 	json.NewEncoder(w).Encode(sleepRequest.SleepData)
 }
@@ -59,4 +76,53 @@ func sleepNewHandler(w http.ResponseWriter, r *http.Request) {
 func sleepDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	fmt.Fprintf(w, "DELETE with id %s", id)
+}
+
+func sleepStartHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET sleep start")
+	var sleep models.Sleep
+	err := sleep.Start()
+	if err != nil {
+		log.Println(err.Error())
+		if err == models.ExistingStartErr {
+			http.Error(w, err.Error(), http.StatusConflict)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	sleep.Save()
+	fmt.Fprintf(w, "started Sleep")
+	return
+}
+
+func sleepEndHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET sleep end")
+	var sleep models.Sleep
+	err := sleep.End()
+	if err != nil {
+		if err == models.NoExistingSessionErr {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	sleep.Save()
+	fmt.Fprintf(w, "ended Sleep")
+}
+
+func sleepToggleStatus(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET sleep toggle status")
+	var sleep models.Sleep
+	ok, err := sleep.Status()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if ok {
+		fmt.Fprintf(w, "sleep session active")
+		return
+	}
+	http.Error(w, "not found", http.StatusNotFound)
+	return
 }
--- models/sleep.go
+++ models/sleep.go
@@ -10,20 +10,65 @@ import (
 
 //Sleep - tracks the baby's sleep start and end.
 type Sleep struct {
-	ID         string    `json:"id" gorethink:"id"`
+	ID         string    `json:"id" gorethink:"id,omitempty"`
 	SleepStart time.Time `json:"start" gorethink:"start"`
 	SleepEnd   time.Time `json:"end" gorethink:"end"`
 	OwnerID    string    `json:"user_id" gorethink:"user_id"`
 }
 
-//StartSleep - record start of sleep
-func (sleep *Sleep) StartSleep() {
-	sleep.SleepStart = time.Now()
+var ExistingStartErr = errors.New("already have a start record")
+var NoExistingSessionErr = errors.New("no existing sleep session to end")
+
+func (sleep *Sleep) Status() (bool, error) {
+	session, err := GetConnection()
+	if err != nil {
+		return false, err
+	}
+	defer session.Close()
+	//check to see if we already have an open sleep session
+	res, err := gorethink.Table("sleep").Filter(map[string]interface{}{
+		"end": time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC),
+	}).Run(session)
+	if err != nil {
+		return false, err
+	}
+	err = res.One(&sleep)
+	if err != nil {
+		//if we don't, then set the sleep start as now and return
+		if err == gorethink.ErrEmptyResult {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
 
-//EndSleep - record end of sleep
-func (sleep *Sleep) EndSleep() {
-	sleep.SleepEnd = time.Now()
+//Start - record start of sleep
+func (sleep *Sleep) Start() error {
+	ok, err := sleep.Status()
+	if err != nil {
+		return err
+	}
+	if !ok {
+		sleep.SleepStart = time.Now()
+		return nil
+	}
+	return ExistingStartErr
+
+}
+
+//End - record end of sleep
+func (sleep *Sleep) End() error {
+	ok, err := sleep.Status()
+	if err != nil {
+		return err
+	}
+	if ok {
+		sleep.SleepEnd = time.Now()
+		return nil
+	}
+	return NoExistingSessionErr
+
 }
 
 //Save - creates/saves the record.  saves if there is an id filled in.
@@ -33,17 +78,37 @@ func (sleep *Sleep) Save() error {
 		return err
 	}
 	defer session.Close()
-	log.Printf("sleep: %v", sleep.OwnerID)
-	if sleep.OwnerID != "" {
-		resp, err := gorethink.Table("sleep").Insert(sleep, gorethink.InsertOpts{Conflict: "replace"}).RunWrite(session)
-		if err != nil {
-			log.Println("error with upsert from sleep upsert in sleep.Save()")
-			return err
-		}
-		if resp.Inserted > 0 {
-			sleep.ID = resp.GeneratedKeys[0]
-		}
+	// log.Printf("sleep: %v", sleep.OwnerID)
+	// if sleep.OwnerID != "" {
+	resp, err := gorethink.Table("sleep").Insert(sleep, gorethink.InsertOpts{Conflict: "replace"}).RunWrite(session)
+	if err != nil {
+		log.Println("error with upsert from sleep upsert in sleep.Save()")
+		return err
 	}
+	if resp.Inserted > 0 {
+		sleep.ID = resp.GeneratedKeys[0]
+	}
+	// }
 
-	return errors.New("an owner should be included")
+	// return errors.New("an owner should be included")
+	return nil
+}
+
+func (sleep *Sleep) GetAll() ([]Sleep, error) {
+	session, err := GetConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+	resp, err := gorethink.Table("sleep").OrderBy(gorethink.Desc("start")).Run(session)
+	if err != nil {
+		return nil, err
+	}
+	var rows []Sleep
+	err = resp.All(&rows)
+	if err != nil {
+		log.Println("error getting all")
+		return nil, err
+	}
+	return rows, nil
 }
