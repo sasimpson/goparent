81ed5cd5df7ad940fe4c76474e0dd8c146c9fa36
Scott Simpson <sasimpson@gmail.com>
2018-11-05 16:15:41 -0800

    fixed tests.  adding save to feeding

--- datastore/feeding.go
+++ datastore/feeding.go
@@ -43,7 +43,26 @@ func (s *FeedingService) Save(ctx context.Context, feeding *goparent.Feeding) er
 
 //Feeding -
 func (s *FeedingService) Feeding(ctx context.Context, family *goparent.Family, days uint64) ([]*goparent.Feeding, error) {
-	panic("not implemented")
+	var feedings []*goparent.Feeding
+	familyKey := datastore.NewKey(ctx, FamilyKind, family.ID, 0, nil)
+
+	daysBack := int(0 - days)
+	start := time.Now().AddDate(0, 0, daysBack)
+
+	q := datastore.NewQuery(FeedingKind).Ancestor(familyKey).Filter("TimeStamp > ", start).Order("-TimeStamp")
+	itx := q.Run(ctx)
+	for {
+		var feeding goparent.Feeding
+		_, err := itx.Next(&feeding)
+		if err == datastore.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		feedings = append(feedings, &feeding)
+	}
+	return feedings, nil
 }
 
 //Stats -
